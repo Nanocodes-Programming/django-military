762f7bc17c0e84561af568a967a4ac0d17a9eed1
runtee <ekwo.chinonso.01@gmail.com>
2023-09-01 12:07:13 +0100

    deploy

--- military/settings.py
+++ military/settings.py
@@ -90,7 +90,7 @@ SIMPLE_JWT = {
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
