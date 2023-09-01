e7d1b6480cabf60055db410853c9a5ed8d485545
runtee <ekwo.chinonso.01@gmail.com>
2023-09-01 15:06:06 +0100

    user dev

--- accounts/models.py
+++ accounts/models.py
@@ -12,15 +12,15 @@ class CustomUserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, username, password=None, **extra_fields):
+    def create_superuser(self, username, password=None, **extra_fields):
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_staff', True)        
-        return self.create_user(email, username, password, **extra_fields)
+        return self.create_user(username, password, **extra_fields)
 
 class CustomUser(BaseModel, AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=30, unique=True)
-    phone_number = models.CharField(max_length=20)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
     is_suspended = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
