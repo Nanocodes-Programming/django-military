3931bfe02a45f8cbae14ebfa4347042c1befffb9
runtee <ekwo.chinonso.01@gmail.com>
2023-09-19 13:04:47 +0100

    support

--- support/urls.py
+++ support/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import IssueCategoryViewSet, UserSupportIssueViewSet, SupportInfoSerializer
+from .views import IssueCategoryViewSet, UserSupportIssueViewSet, SupportInfoViewSet
 
 router = DefaultRouter()
 router.register(r'issue-categories', IssueCategoryViewSet)
 router.register(r'user-support-issues', UserSupportIssueViewSet)
-router.register(r'support', SupportInfoSerializer)
+router.register(r'support', SupportInfoViewSet)
 urlpatterns = [
     path('', include(router.urls)),
 ]
