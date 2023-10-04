55d7ae7bca99b24da9e9cdf035cffa2e5cbe84b2
runtee <ekwo.chinonso.01@gmail.com>
2023-10-04 14:20:32 +0100

    fix

--- activityLog/serializers.py
+++ activityLog/serializers.py
@@ -2,13 +2,13 @@ from rest_framework import serializers
 from .models import ActivityLog
 from accounts.models import CustomUser
 
-class CustomUserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ["id","email","username","is_staff",]
 
 class ActivityLogSerializer(serializers.ModelSerializer):
-    user = CustomUserSerializer(read_only=True)
+    user = UserSerializer(read_only=True)
     class Meta:
         model = ActivityLog
         fields = '__all__'
